4d77d4fc0624a5c95ddbe2c93c8a90007b0e8b26
rakib-09 <rakib.rumi09@gmail.com>
2022-06-17 06:04:40 +0600

    ➕ Add redirection helper method
    Type(s): ADD

--- helpers/helper_functions.php
+++ helpers/helper_functions.php
@@ -6,3 +6,11 @@ if (!function_exists('root_path')) {
         return dirname(__DIR__);
     }
 }
+
+if (!function_exists('redirect')) {
+    function redirect(string $location): string
+    {
+        header("Location: $location");
+        exit();
+    }
+}
